893dc685e37d90efcc6db8f65c1c6bf906081e97
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-06-24 12:59:22 +0300

    Properties for native app

--- autoservice-app-android-tests/src/test/java/tests/web/WebMobileAppTests.java
+++ autoservice-app-android-tests/src/test/java/tests/web/WebMobileAppTests.java
@@ -1,28 +1,28 @@
-package tests.web;
-
-import application.web.WebMobileApp;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-public class WebMobileAppTests {
-
-    static WebMobileApp webMobileApp = new WebMobileApp();
-
-    @BeforeClass
-    public static void init() {
-        webMobileApp.startApp();
-        webMobileApp.navigate().toOrderPage();
-    }
-
-    @Test
-    public void simplestWebTest() {
-
-    }
-
-    @AfterClass
-    public static void deinit() {
-        webMobileApp.stopApp();
-    }
-
-}
+//package tests.web;
+//
+//import application.web.WebMobileApp;
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//public class WebMobileAppTests {
+//
+//    static WebMobileApp webMobileApp = new WebMobileApp();
+//
+//    @BeforeClass
+//    public static void init() {
+//        webMobileApp.startApp();
+//        webMobileApp.navigate().toOrderPage();
+//    }
+//
+//    @Test
+//    public void simplestWebTest() {
+//
+//    }
+//
+//    @AfterClass
+//    public static void deinit() {
+//        webMobileApp.stopApp();
+//    }
+//
+//}

96579806e1867745dec882e5ee2245e61e880321
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-06-25 15:11:37 +0300

    Get properties from gradle parameters -D

--- autoservice-app-android-tests/src/test/java/tests/nat/NativeAppTests.java
+++ autoservice-app-android-tests/src/test/java/tests/nat/NativeAppTests.java
@@ -27,46 +27,46 @@ public class NativeAppTests {
         assertions.assertAll();
     }
 
-//    @Test
-//    public void findOrderByIdTest() {
-//        nativeApp.ordersMenu().toFindOne();
-//        SoftAssertions assertions = new SoftAssertions();
-//        assertions.assertThat(nativeApp.findOneActivity().getOrders()).contains("Id:");
-//        nativeApp.findOneActivity().back();
-//        assertions.assertAll();
-//    }
-//
-//    @Test
-//    public void findAllOrdersTest() {
-//        nativeApp.ordersMenu().toFindAll();
-//        SoftAssertions assertions = new SoftAssertions();
-//        String text = nativeApp.findAllActivity().getOrders();
-//        assertions.assertThat(text).contains("Id:");
-//        nativeApp.findAllActivity().back();
-//        assertions.assertAll();
-//    }
-//
-//    @Test
-//    public void updateOrderTest() {
-//        nativeApp.ordersMenu().toUpdate();
-//        SoftAssertions assertions = new SoftAssertions();
-//        assertions.assertThat(nativeApp.updateOrderActivity().getHeader()).contains("Update Order");
-//        nativeApp.updateOrderActivity().fillId("1").fillDate("28.02.2007").fillOwnerName("Test Owner");
-//        assertions.assertThat(nativeApp.updateOrderActivity().isKeyboardShown()).isTrue();
-//        nativeApp.updateOrderActivity().closeKeyboard().submit().back();
-//        assertions.assertAll();
-//    }
-//
-//    @Test
-//    public void deleteOrderTest() {
-//        nativeApp.ordersMenu().toDelete();
-//        SoftAssertions assertions = new SoftAssertions();
-//        assertions.assertThat(nativeApp.deleteOrderActivity().getHeader()).contains("Delete Order");
-//        nativeApp.deleteOrderActivity().fillId("1");
-//        assertions.assertThat(nativeApp.deleteOrderActivity().isKeyboardShown()).isTrue();
-//        nativeApp.deleteOrderActivity().closeKeyboard().submit().back();
-//        assertions.assertAll();
-//    }
+    @Test
+    public void findOrderByIdTest() {
+        nativeApp.ordersMenu().toFindOne();
+        SoftAssertions assertions = new SoftAssertions();
+        assertions.assertThat(nativeApp.findOneActivity().getOrders()).contains("Id:");
+        nativeApp.findOneActivity().back();
+        assertions.assertAll();
+    }
+
+    @Test
+    public void findAllOrdersTest() {
+        nativeApp.ordersMenu().toFindAll();
+        SoftAssertions assertions = new SoftAssertions();
+        String text = nativeApp.findAllActivity().getOrders();
+        assertions.assertThat(text).contains("Id:");
+        nativeApp.findAllActivity().back();
+        assertions.assertAll();
+    }
+
+    @Test
+    public void updateOrderTest() {
+        nativeApp.ordersMenu().toUpdate();
+        SoftAssertions assertions = new SoftAssertions();
+        assertions.assertThat(nativeApp.updateOrderActivity().getHeader()).contains("Update Order");
+        nativeApp.updateOrderActivity().fillId("1").fillDate("28.02.2007").fillOwnerName("Test Owner");
+        assertions.assertThat(nativeApp.updateOrderActivity().isKeyboardShown()).isTrue();
+        nativeApp.updateOrderActivity().closeKeyboard().submit().back();
+        assertions.assertAll();
+    }
+
+    @Test
+    public void deleteOrderTest() {
+        nativeApp.ordersMenu().toDelete();
+        SoftAssertions assertions = new SoftAssertions();
+        assertions.assertThat(nativeApp.deleteOrderActivity().getHeader()).contains("Delete Order");
+        nativeApp.deleteOrderActivity().fillId("1");
+        assertions.assertThat(nativeApp.deleteOrderActivity().isKeyboardShown()).isTrue();
+        nativeApp.deleteOrderActivity().closeKeyboard().submit().back();
+        assertions.assertAll();
+    }
 
     @AfterClass
     public static void deinit() {
